c14d0a4eb96685a4b98715b0de082bb4d69e89ba
MiloMT <myles@mylestaylor.dev>
2024-02-20 13:31:53 +1000

    Added user route validation and corrected port for deployment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,40 @@
 import mongoose from 'mongoose'
 
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+}
+
 const usersSchema = new mongoose.Schema({
-    name: { type: String, required: true }
+    name: { 
+        type: String, 
+        trim: true,
+        required: [true, "Name is required"],
+        minlength: [3, "The name must be at least 3 characters long"],
+        maxlength: [50, "The name can be a maximum of 50 characters long"]
+    },
+    email: { 
+        type: String,
+        trim: true,
+        minlength: [5, "The Email address must be at least 5 characters long"],
+        maxlength: [100, "The Email address can be a maximum of 100 characters long"],
+        required: [true, "Email address is required"],
+        unique: [true, "An account already exists with that email"],
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    language: { 
+        type: String
+    },
+    streamingPlatform: [{ 
+        type: String 
+    }],
+    watchList: [{ 
+        type: String 
+    }],
+    wishList: [{ 
+        type: String 
+    }],
 })
 
 export default usersSchema
