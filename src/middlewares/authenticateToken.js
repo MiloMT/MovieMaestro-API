a07e05aec2cd44d4551daa95cc4f018fb2304810
MiloMT <myles@mylestaylor.dev>
2024-02-21 11:12:11 +1000

    Added error message if no bearer token used

--- src/middlewares/authenticateToken.js
+++ src/middlewares/authenticateToken.js
@@ -4,7 +4,9 @@ function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"]
     const token = authHeader && authHeader.split(" ")[1]
     if (token == null) {
-        return res.sendStatus(401)
+        return res.status(401).send({ 
+            error: "You need to login to use this service" 
+        })
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
